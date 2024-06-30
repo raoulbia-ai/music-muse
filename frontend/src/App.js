502cd4afb167a3f2266eeff6fafadb7a9c4fa0c6
Raoul Biagioni <raoulbia@gmailcom>
2024-06-30 12:31:57 +0100

    Tidied up code, add comments and make PEP8 compliant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,23 @@
+// Import React and useState hook
 import React, { useState } from 'react';
+// Import SearchBox component
 import SearchBox from './SearchBox';
+// Import stylesheet
 import './index.css';
 
+// Defines the main App component
 const App = () => {
+  // State hook for managing selected details
   const [details, setDetails] = useState(null);
 
+  // Renders the App component
   return (
     <div className="app">
       <h1>Music Muse</h1>
       <p>Welcome! Use the search box below to explore Albums and Songs of your favorite artists. Start typing to see suggestions and select to view detailed information.</p>
+      {/* Pass setDetails function as a prop to SearchBox */}
       <SearchBox setDetails={setDetails} />
+      {/* Conditionally render the results table if details are available */}
       {details && (
         <table className="results-table">
           <thead>
@@ -19,17 +27,3 @@ const App = () => {
               <th>Song Title</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>{details.artist}</td>
-              <td>{details.album}</td>
-              <td>{details.song}</td>
-            </tr>
-          </tbody>
-        </table>
-      )}
-    </div>
-  );
-};
-
-export default App;
