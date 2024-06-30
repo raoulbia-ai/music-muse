1b5602c24d5f9c0bb431d7e172c693867a4f1116
Raoul Biagioni <raoulbia@gmailcom>
2024-06-30 12:29:14 +0100

    Tidied up code, add comments and make PEP8 compliant

--- backend/app.py
+++ backend/app.py
@@ -3,49 +3,47 @@ from flask_cors import CORS
 import json
 import os
 
+# Initialize Flask app
 app = Flask(__name__)
+
+# Enable CORS for all domains on all routes
 CORS(app, resources={r"/*": {"origins": ["https://jolly-sky-0071d7d03.5.azurestaticapps.net"]}})
 
+# Load data from JSON file
 with open('data.json', 'r') as f:
     data = json.load(f)
 
 @app.route('/', methods=['GET'])
 def root():
+    """Root endpoint returning a welcome message."""
     return jsonify({"message": "Welcome to the application!"}), 200
 
 @app.route('/health', methods=['GET'])
 def health_check():
+    """Health check endpoint to indicate the application is up and running."""
     return jsonify({"Application status": "up and running"}), 200
 
 @app.route('/api/suggestions', methods=['GET'])
 def get_suggestions():
+    """Endpoint to get artist suggestions based on a query."""
     query = request.args.get('query', '')
-    suggestions = []
-
-    for artist in data:
-        if query.lower() in artist['name'].lower():
-            suggestions.append(artist['name'])
-    
+    suggestions = [artist['name'] for artist in data if query.lower() in artist['name'].lower()]
     return jsonify(suggestions)
 
 @app.route('/api/all_artists', methods=['GET'])
 def get_all_artists():
-    artists = [artist['name'] for artist in data]
-    artists.sort()
+    """Endpoint to get a sorted list of all artists."""
+    artists = sorted(artist['name'] for artist in data)
     return jsonify(artists)
 
 @app.route('/api/details', methods=['GET'])
 def get_details():
+    """Endpoint to get details of an artist based on a query."""
     query = request.args.get('query', '')
-    details = {}
-
-    for artist in data:
-        if query.lower() == artist['name'].lower():
-            details = artist
-            break
-    
+    details = next((artist for artist in data if query.lower() == artist['name'].lower()), {})
     return jsonify(details)
 
 if __name__ == "__main__":
+    # Run the Flask app on port from environment or default to 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
